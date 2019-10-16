e2ce1884518619815784f0be60a8cbc9b2dcc23f
udaydutt12 <udaydutt@utexas.edu>
2019-10-16 02:04:17 -0500

    start working on the representation of the state of the board

---
+++ State.cpp
@@ -0,0 +1,24 @@
+
+#include <vector>
+
+
+
+State::State() {
+
+}
+
+    std::vector<std::vector<std::vector<int> > > serialize();
+    std::vector<int> edges();
+
+State::std::vector<std::vector<std::vector<int> > >serialize() {
+
+}
+
+
+State::______ key() {
+
+
+    
+}
+
+
---
+++ State.h
@@ -0,0 +1,10 @@
+
+#include <vector>
+
+class State {
+    State();
+    std::vector<std::vector<std::vector<int> > > serialize();
+    ____ key();
+    std::vector<int> edges();
+
+};
